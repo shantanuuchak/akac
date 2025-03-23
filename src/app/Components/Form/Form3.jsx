8c590470c0bc10e2e18aa520f8a0a6b9c48682db
shantanuuchak <51011922+shantanuuchak@users.noreply.github.com>
2025-03-23 22:46:43 +0000

    Test google form integration

--- src/app/Components/Form/Form3.jsx
+++ src/app/Components/Form/Form3.jsx
@@ -1,11 +1,11 @@
 const Form3 = () => {
     return (
-        <form action="#" className="cs_contact_form">
+        <form action="https://docs.google.com/forms/d/e/1FAIpQLSc7a-eJIYhkiHEoo2LHUiAbbVIPmq2XOcosIeR024amFQoP3g/formResponse" method="POST" className="cs_contact_form">
             <h2 className="text-center cs_fs_36 cs_semibold">Book An Appointment</h2>
-            <input type="text" className="cs_form_field cs_mb_22" placeholder="Your Name" />
-            <input type="email" className="cs_form_field cs_mb_22" placeholder="Your Email" />
-            <input type="text" className="cs_form_field cs_mb_22" placeholder="Select Subjects" />
-            <textarea cols="30" rows="4" className="cs_form_field cs_mb_22" placeholder="Write Message..."></textarea>
+            <input type="text" className="cs_form_field cs_mb_22" placeholder="Your Name" name="entry.455049253" />
+            <input type="email" className="cs_form_field cs_mb_22" placeholder="Your Email" name="entry.240261482" />
+            <input type="text" className="cs_form_field cs_mb_22" placeholder="Select Subjects" name="entry.1641905770" />
+            <textarea cols="30" rows="4" className="cs_form_field cs_mb_22" placeholder="Write Message..." name="entry.2118204743"></textarea>
             <button className="cs_btn cs_style_1 w-100" type="submit"><span>Appointment Now</span></button>
         </form>
     );
