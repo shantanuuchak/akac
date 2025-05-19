b5804ec19f2978018230d5f2fe9984b2a81aab3a
shantanuuchak <51011922+shantanuuchak@users.noreply.github.com>
2025-05-19 16:23:09 +0530

    Remove blog pages

--- src/app/Components/Services/Cta.jsx
+++ src/app/Components/Services/Cta.jsx
@@ -1,39 +1,64 @@
-"use client"
+"use client";
 import { useEffect } from "react";
 import loadBackgroudImages from "../Common/loadBackgroudImages";
 import Link from "next/link";
 import Image from "next/image";
 
 const Cta = () => {
+  useEffect(() => {
+    loadBackgroudImages();
+  }, []);
 
-    useEffect(() => {
-        loadBackgroudImages();
-      }, []);
-
-    return (
-        <section className="cs_cta cs_style_1 cs_bg_filed" data-background="/assets/img/cta_bg_1.svg">
-        <div className="container">
-          <div className="cs_cta_in">
-            <div className="cs_section_heading cs_style_1">
-              <h3 className="cs_section_subtitle cs_white_color text-uppercase cs_medium cs_fs_20 cs_mb_10">
-              <Image src="/assets/img/icons/fan.svg" className="cs_section_subheading_icon" alt="img" width={15} height={15}   />
-                Free to Contact Us
-              </h3>
-              <h2 className="cs_section_title cs_white_color cs_fs_48 cs_semibold mb-0">Get Premium Air Services <br/>From With Us!</h2>
-            </div>
-            <Link href="/contact" className="cs_btn cs_style_1 cs_color_1">
-              <span>describe more</span>
-              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M8.28125 0.71875L13.7812 5.96875C13.9271 6.11458 14 6.29167 14 6.5C14 6.70833 13.9271 6.88542 13.7812 7.03125L8.28125 12.2812C7.90625 12.5729 7.55208 12.5729 7.21875 12.2812C6.92708 11.9062 6.92708 11.5521 7.21875 11.2188L11.375 7.25H0.75C0.291667 7.20833 0.0416667 6.95833 0 6.5C0.0416667 6.04167 0.291667 5.79167 0.75 5.75H11.375L7.21875 1.78125C6.92708 1.44792 6.92708 1.09375 7.21875 0.71875C7.55208 0.427083 7.90625 0.427083 8.28125 0.71875Z" fill="currentColor"></path>
-              </svg>                
-            </Link>
+  return (
+    <section
+      className="cs_cta cs_style_1 cs_bg_filed"
+      data-background="/assets/img/cta_bg_1.svg"
+    >
+      <div className="container">
+        <div className="cs_cta_in">
+          <div className="cs_section_heading cs_style_1">
+            <h3 className="cs_section_subtitle cs_white_color text-uppercase cs_medium cs_fs_20 cs_mb_10">
+              <Image
+                src="/assets/img/icons/fan.svg"
+                className="cs_section_subheading_icon"
+                alt="img"
+                width={15}
+                height={15}
+              />
+              Free to Contact Us
+            </h3>
+            <h2 className="cs_section_title cs_white_color cs_fs_48 cs_semibold mb-0">
+              Get Premium Air Services <br />
+              From With Us!
+            </h2>
           </div>
+          <Link href="/contact" className="cs_btn cs_style_1 cs_color_1">
+            <span>Contact Now</span>
+            <svg
+              width="14"
+              height="13"
+              viewBox="0 0 14 13"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8.28125 0.71875L13.7812 5.96875C13.9271 6.11458 14 6.29167 14 6.5C14 6.70833 13.9271 6.88542 13.7812 7.03125L8.28125 12.2812C7.90625 12.5729 7.55208 12.5729 7.21875 12.2812C6.92708 11.9062 6.92708 11.5521 7.21875 11.2188L11.375 7.25H0.75C0.291667 7.20833 0.0416667 6.95833 0 6.5C0.0416667 6.04167 0.291667 5.79167 0.75 5.75H11.375L7.21875 1.78125C6.92708 1.44792 6.92708 1.09375 7.21875 0.71875C7.55208 0.427083 7.90625 0.427083 8.28125 0.71875Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </Link>
         </div>
-        <div className="cs_cta_img">
-          <Image src="/assets/img/cta_img_1.png" alt="img" width={571} height={326}   />
-          </div>
-      </section>
-    );
+      </div>
+      <div className="cs_cta_img">
+        <Image
+          src="/assets/img/cta_img_1.png"
+          alt="img"
+          width={571}
+          height={326}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Cta;
--- src/app/page.js
+++ src/app/page.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import HomeSlider2 from './Components/Home2/HomeSlider2';
-import About2 from './Components/About/About2';
-import Services2 from './Components/Home2/Services2';
-import Choose2 from './Components/Home2/Choose2';
-import Cta from './Components/Services/Cta';
-import Contact2 from './Components/Home2/Contact2';
-import Team from './Components/Home2/Team';
-import Project2 from './Components/Home2/Project2';
-import Testimonial3 from './Components/Home2/Testimonial3';
-import Blog2 from './Components/Home2/Blog2';
+import React from "react";
+import HomeSlider2 from "./Components/Home2/HomeSlider2";
+import About2 from "./Components/About/About2";
+import Services2 from "./Components/Home2/Services2";
+import Choose2 from "./Components/Home2/Choose2";
+import Cta from "./Components/Services/Cta";
+import Contact2 from "./Components/Home2/Contact2";
+// import Team from './Components/Home2/Team';
+import Project2 from "./Components/Home2/Project2";
+import Testimonial3 from "./Components/Home2/Testimonial3";
+// import Blog2 from "./Components/Home2/Blog2";
 
 const page = () => {
-    return (
-        <div className='home-page2'>
-            <HomeSlider2></HomeSlider2>
-            <About2 
-                Img="/assets/img/about_img_2.png"
-                TitleUp="About us"
-                Title="Turning up the chill factor in your home or office"
-                Content="AKAC, established by Arun Kumar, is a name synonymous with excellence in air conditioning, washing machine, and refrigerator services. Based in mumbai, we combine technical mastery with a customer-first approach to deliver reliable solutions."
-                FeatureList={[
-                    "Superior air conditioning solutions",
-                    "Optimized laundry system care",
-                    "Reliable refrigeration support"
-                ]}
-                Experience="14+"
-                BioImg="/assets/img/avatar_1.png"                
-                BioName="Arun Kumar"                
-                BioContent="Call any time for services"                
-            ></About2>
-            <Services2></Services2>
-            <Choose2></Choose2>
-            <Cta></Cta>
-            {/* <Team></Team> */}
-            <Contact2></Contact2>
-            <Project2></Project2>
-            <Testimonial3></Testimonial3>
-            <Blog2></Blog2>
-        </div>
-    );
+  return (
+    <div className="home-page2">
+      <HomeSlider2></HomeSlider2>
+      <About2
+        Img="/assets/img/about_img_2.png"
+        TitleUp="About us"
+        Title="Turning up the chill factor in your home or office"
+        Content="AKAC, established by Arun Kumar, is a name synonymous with excellence in air conditioning, washing machine, and refrigerator services. Based in mumbai, we combine technical mastery with a customer-first approach to deliver reliable solutions."
+        FeatureList={[
+          "Superior air conditioning solutions",
+          "Optimized laundry system care",
+          "Reliable refrigeration support",
+        ]}
+        Experience="14+"
+        BioImg="/assets/img/avatar_1.png"
+        BioName="Arun Kumar"
+        BioContent="Call any time for services"
+      ></About2>
+      <Services2></Services2>
+      <Choose2></Choose2>
+      <Cta></Cta>
+      {/* <Team></Team> */}
+      <Contact2></Contact2>
+      <Project2></Project2>
+      <Testimonial3></Testimonial3>
+      {/* <Blog2></Blog2> */}
+    </div>
+  );
 };
 
 export default page;
